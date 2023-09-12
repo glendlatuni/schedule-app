350fde63ead0b16f26d10206497e74c674984fab
glendlatuni <estetikaglend@gmail.com>
2023-09-12 17:09:38 +0900

    tes_utama

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,19 +1,22 @@
 async function getData(id) {
   try {
-    const res = await fetch(`http://localhost:4000/jadwal/${id}`, {
+    const res = await fetch(`http://localhost:4000/jadwal/`, {
       next: {
         revalidate: 0,
       },
     });
     return res.json();
   } catch (error) {
-    throw new error(error);
+    console.log("ERROR");
   }
 }
 
-export default async function dynamicPage({ params }) {
+// eslint-disable-next-line @next/next/no-async-client-component
+export default async function DynamicPage({ params }) {
   const jadwals = await getData(params.id);
-  console.log(jadwals);
+
+  const jadwalFilter = jadwals.filter((jadwalz) => jadwalz.id === params.id);
+  console.log(jadwalFilter);
 
   return (
     <div className="flex flex-col items-center">
@@ -23,7 +26,7 @@ export default async function dynamicPage({ params }) {
       </h1>
       <table className=" min-w-full table-auto ">
         <thead className="text-sm font-light ">
-          <tr className=" bg-red-500 lg:bg-slate-700">
+          <tr className=" bg-red-500 lg:`bg-slate-700`">
             <th className="text-sm font-light px-2 py-2">
               <span className="text-gray-200">Name</span>
             </th>
@@ -41,34 +44,36 @@ export default async function dynamicPage({ params }) {
             </th>
           </tr>
         </thead>
-        <tbody>
-          {/* isi tabel  */}
-
-          {jadwals.jadwal &&
-            jadwals.jadwal.map((itemTabel) => (
-              // eslint-disable-next-line react/jsx-key
-              <tr>
-                <th className="text-sm font-light px-2 py-2">
-                  <span className="text-gray-900">{itemTabel.nama}</span>
-                </th>
-                <th className="text-sm font-light px-2 py-2">
-                  <span className="text-gray-900">{itemTabel.alamat}</span>
-                </th>
-                <th className="text-sm font-light px-2 py-2">
-                  <span className="text-gray-900">
-                    {itemTabel.hari_tanggal}
-                  </span>
-                </th>
-                <th className="text-sm font-light px-2 py-2">
-                  <span className="text-gray-900">{itemTabel.Pelayan}</span>
-                </th>
-                <th className="text-sm font-light px-2 py-2">
-                  <span className="text-gray-900">{itemTabel.Liturgi}</span>
-                </th>
-              </tr>
-            ))}
-        </tbody>
+        {jadwalFilter.map((jadwalx, index) => (
+          <tbody key={index}>
+            <tr>
+              <th className="text-sm font-light px-2 py-2">
+                <span className="text-gray-900">{jadwalx.nama}</span>
+              </th>
+              <th className="text-sm font-light px-2 py-2">
+                <span className="text-gray-900">{jadwalx.alamat}</span>
+              </th>
+              <th className="text-sm font-light px-2 py-2">
+                <span className="text-gray-900">{jadwalx.hari_tanggal}</span>
+              </th>
+              <th className="text-sm font-light px-2 py-2">
+                <span className="text-gray-900">{jadwalx.Pelayan}</span>
+              </th>
+              <th className="text-sm font-light px-2 py-2">
+                <span className="text-gray-900">{jadwalx.Liturgi}</span>
+              </th>
+            </tr>
+          </tbody>
+        ))}
       </table>
+
+      {/* <div>
+        {jadwalFilter.map((jadwalx, index) => (
+          <div key={index}>
+            <p>{jadwalx.hari_tanggal}</p>
+          </div>
+        ))}
+      </div> */}
     </div>
   );
 }
